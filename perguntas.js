7ae2023ad61a9c9faf9457c48f1c6a84b848ac9e
Guilherme Gasparini Lovatto <guilherme.lovatto@outlook.com>
2025-05-26 19:13:26 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript',
+    'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Programação',
+    'O que é HTML?',
+    'Linguagem de marcação para estruturar páginas web.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é CSS?',
+    'Linguagem para estilizar documentos HTML.'
+)
+
+criaCartao(
+    'Programação',
+    'O que significa DOM?',
+    'Modelo de objeto do documento.'
+)
+
+criaCartao(
+    'Programação',
+    'Para que serve uma função em JavaScript?',
+    'Reutilizar blocos de código.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma variável?',
+    'Espaço para armazenar valores.'
+)
